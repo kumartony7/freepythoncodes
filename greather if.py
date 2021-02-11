65a18a675bd7b34fd3d56e16e51da41952759325
kumartony7 <78829232+kumartony7@users.noreply.github.com>
2021-02-11 13:18:14 +0530

    Update greather if.py

--- greather if.py
+++ greather if.py
@@ -1,9 +1,8 @@
-first number =input('Enter first value')
-first  = int(first number)
-second number =input('Enter second value')
-second=int(second number)
+first_number =input('Enter first value')
+first  =float(first_number)
+second_number =input('Enter second value')
+second=float(second_number)
 if first>second:
-    print('1st  input is grether than second input')
+    print('1st  value is grether than second value')
 else:
-    print('2nd input is greather than 1st input')
-
+    print('2nd value is greather than 1st value')
