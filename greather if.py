a16d5f54782f2a79d423490eae4adacfa264179f
kumartony7 <78829232+kumartony7@users.noreply.github.com>
2021-02-11 13:12:35 +0530

    Add files via upload

---
+++ greather if.py
@@ -0,0 +1,9 @@
+first=input('first value')
+st = int(first)
+second=input('second value')
+nd=int(second)
+if st>nd:
+    print('1st is grether than second')
+else:
+    print('2nd is greather than 1st')
+
